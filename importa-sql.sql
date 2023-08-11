745c2be6bd7a380aec955b91507f416e297cfeb4
B1C4 <lucasbicalleto2@gmail.com>
2023-08-11 12:54:47 -0300

    Create importa-sql.sql

---
+++ importa-sql.sql
@@ -0,0 +1,8 @@
+-- Criação do banco de dados
+CREATE DATABASE pixpay;
+
+-- Seleciona o banco de dados
+USE pixpay;
+
+-- Importa os dados do arquivo .sql
+source /root/PixPayCTF/pixpay-ctf/pixpay.sql;
