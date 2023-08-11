128358563546caac7d650e43099c990d4cbc2d35
B1C4 <lucasbicalleto2@gmail.com>
2023-08-11 12:52:15 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -1,14 +1,7 @@
 <?php
 
-$servername = "localhost";
+$servername = "mysql";
 $username   = "root";
-$password   = "";
+$password   = "RS827vCaPo21xW3Zvla09SAez";
 $dbname     = "pixpay";
-
-// if (!isset($_ENV['localhost']) || $_ENV['localhost'] != true) {
-    $servername = "149.100.155.1";
-    $username   = "u666788042_pixpay";
-    $password   = "PixPay2023";
-    $dbname     = "u666788042_pixpay";
-// }
 ?>
